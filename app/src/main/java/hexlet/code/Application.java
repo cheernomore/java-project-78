0b8f0753242cbc1f49abb75cf8d7777f77a67f71
ch33rnomore <ch33rnomore@gmail.com>
2024-06-15 11:37:21 +0300

    Project restructured, change build.gradle to build.gradle.kts

--- app/src/main/java/hexlet/code/Application.java
+++ app/src/main/java/hexlet/code/Application.java
@@ -3,8 +3,6 @@
  */
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-
-    }
+public class Application {
+    public static void main(String[] args) {}
 }
--- app/src/test/java/hexlet/code/ApplicationTest.java
+++ app/src/test/java/hexlet/code/ApplicationTest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AppTest {
+class ApplicationTest {
     @Test
     @DisplayName("Проверка проверяемости длины строки")
     void validVerifyStringLength() {
