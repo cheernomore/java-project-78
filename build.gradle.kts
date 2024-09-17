82ad9f4fff9ede5600224d6d6f501935619f881e
ch33rnomore <ch33rnomore@gmail.com>
2024-09-17 12:29:37 +0300

    Restructured project #7: to example

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "hexlet.code"
+group = "io.hexlet"
 
 version = "1.0-SNAPSHOT"
 
@@ -24,15 +24,15 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.24.2")
+
 }
 
 tasks.test {
     useJUnitPlatform()
-    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
     testLogging {
         exceptionFormat = TestExceptionFormat.FULL
         events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-         showStackTraces = true
+        // showStackTraces = true
         // showCauses = true
         showStandardStreams = true
     }
